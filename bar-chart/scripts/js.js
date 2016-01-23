bd5417f555bfb4c80c1c5257aa15401d0ad679c1
jessiewillms <willms.jessie@gmail.com>
2016-01-23 17:57:10 -0500

    adds the basic bar chart code

--- bar-chart/scripts/js.js
+++ bar-chart/scripts/js.js
@@ -11,8 +11,8 @@ app.init = function() {
 
         url_key = url_link.substr(39, 44),
         url_start = 'https://spreadsheets.google.com/feeds/list/'
-        url_end = '/1/public/values?alt=json-in-script&callback=?'
-    
+    url_end = '/1/public/values?alt=json-in-script&callback=?'
+
     url = url_start + url_key + url_end;
 
     /*---------------------------------------------------------------
@@ -38,22 +38,96 @@ reformat the data
 ---------------------------------------------------------------*/
 var analyseData = function(data) {
 
-    console.log(data)
 
-    makeChart();
+
+    makeChart(data);
 
 };
 
 /*---------------------------------------------------------------
 make the chart
 ---------------------------------------------------------------*/
-var makeChart = function() {
+var makeChart = function(data) {
 
-    var chart = d3.select(".chart")
-        .data(data)
+    /*---------------------------------------------------------------
+    data
+    ---------------------------------------------------------------*/
+    var entry = data.feed.entry;
 
+    // make object
+    var dataObject = {};
+    var dataArry = [];
    
-    chart.html("Hello, world!");
+    // loop over data to create object
+    for (var i = 0; i < entry.length; i++) {
+        
+        var getData = parseInt(entry[i].gsx$data.$t),
+            getLabel = entry[i].gsx$label.$t;
+
+        dataObject[getLabel] = getData;
+
+        dataArry.push( parseInt(getData) );
+
+    }
+
+    console.log(dataArry);
+
+    /*---------------------------------------------------------------
+    set width + height
+    ---------------------------------------------------------------*/
+    var w = 500,
+        h = 100,
+        p = 1;
+
+    /*---------------------------------------------------------------
+    chart | iPOLITICS RED: #751C1E ***or*** (117,28,30)
+    ---------------------------------------------------------------*/
+    var svg = d3.select('.chart')
+        .append('svg')
+        .attr('width',w)
+        .attr('height',h);
+
+    svg.selectAll('rect')
+        .data(dataArry)
+        .enter()
+        .append('rect')
+        .attr('x',function(d, i) {
+            return i * (w / dataArry.length);
+        })
+        .attr('y', function(d) {
+            return h - (d * 4);
+        })
+        .attr('width',w / dataArry.length - p)
+        .attr('height', function(d) {
+            return (d * 4);
+        })
+        .attr("fill", function(d){
+            return 'rgb(117,28,' + (d * 10) + ')';
+        });
+    
+    /*---------------------------------------------------------------
+    labels
+    ---------------------------------------------------------------*/
+    svg.selectAll('text')
+        .data(dataArry)
+        .enter()
+        .append('text') // add the text, *then* manipulate
+        .text(function(d) {
+            return d;
+        })
+        // positions the text
+        .attr('x', function(d, i) {
+            return i * (w / dataArry.length) + (w / dataArry.length - p) / 2;
+        })
+        .attr('y',function(d){
+            return h - (d * 4) + 14;
+        })
+        // styles the text
+        .attr("font-family", "sans-serif")
+        .attr("font-size", "14px")
+        .attr("fill", "white")
+        .attr("text-anchor", "middle")
+
 
 };
 
